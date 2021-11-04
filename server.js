7bb5c44be1060344ca4c0fa917b5951544fc0568
Brian O'Mile <brianmile10@yahoo.com>
2021-11-04 10:21:31 +0000

    completed challenge

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +24,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+const db = {};
+
+function urlIsValid(url) {
+  return new Promise((resolve, reject) => {
+    let [,pref,site,tail] = url.match(/(.*\/\/)(.*)(\/\?.*)/);
+    dns.lookup(site, (err, t, __) => {
+      err? reject() : resolve(pref+site+tail);
+    })
+  })
+}
+
+app.post("/api/shorturl", (req,res) => {
+  let {url} = req.body;
+  urlIsValid(url)
+  .then((data)=> {
+    let short_url =  Object.keys(db).length+1;
+    let original_url = data;
+    db[short_url] = {original_url, short_url};
+    res.json({original_url, short_url})
+  })
+  .catch(()=> res.json({"error":"invalid url"}))
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  let {short_url} = req.params;
+  let redirect = db[short_url].original_url
+  res.redirect(redirect)
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
